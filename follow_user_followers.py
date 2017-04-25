b2de9907faf35a080811436b984040807480a1b0
Alex Roan <alex.roan@hotmail.com>
2017-04-25 19:42:07 +0100

    Stopped filtering, delayed following and unfollowing on follow_user_followers.py

--- follow_user_followers.py
+++ follow_user_followers.py
@@ -22,7 +22,7 @@ parser.add_argument('-proxy', type=str, help="proxy")
 parser.add_argument('users', type=str, nargs='+', help='users')
 args = parser.parse_args()
 
-bot = Bot()
+bot = Bot(filter_users=False, follow_delay=60, unfollow_delay=60)
 bot.login(username=args.u, password=args.p,
           proxy=args.proxy)
 
